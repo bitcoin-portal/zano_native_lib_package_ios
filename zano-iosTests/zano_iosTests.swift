c094905e9eeaca0fd36830e95ea018f283873ef9
jumpe.katayama <junktym123@gmail.com>
2024-10-04 14:26:02 +0900

    Can run test now

--- zano-iosTests/zano_iosTests.swift
+++ zano-iosTests/zano_iosTests.swift
@@ -19,32 +19,6 @@ class zano_iosTests: XCTestCase {
     }
 
     func testZANOVerssion() throws {
-        XCTAssertEqual(ZanoWallet.get_version(), "2.0.0.336[132d2bf]")
+        XCTAssertEqual(ZanoWallet.get_versionCpp(), "2.0.0.336[132d2bf]")
     }
-    
-    func testHelloWorld() throws {
-        XCTAssertEqual(HelloWorld.test(), "This is a test string from ZanoString.")
-    }
-    
-    func testGetWalletFiles() throws {
-        XCTAssertEqual(ZanoWallet.get_wallet_files().data(using: .utf8)?.base64EncodedString(), "ew0KfQ==")
-    }
-    
-    func testGetLogsBuffer() throws {
-        XCTAssertEqual(ZanoWallet.get_logs_buffer(), "")
-    }
-    
-    func testGetOpenedWallets() throws {
-        XCTAssertEqual(ZanoWallet.get_opened_wallets().data(using: .utf8)?.base64EncodedString(), "ew0KICAiaWQiOiAwLA0KICAianNvbnJwYyI6ICIiLA0KICAicmVzdWx0Ijogew0KICAgICJyZXR1cm5fY29kZSI6ICJVTklOSVRJQUxJWkVEIg0KICB9DQp9")
-    }
-
-    func testTruncateLog() throws {
-        XCTAssertEqual(ZanoWallet.truncate_log().data(using: .utf8)?.base64EncodedString(), "ew0KICAiaWQiOiAwLA0KICAianNvbnJwYyI6ICIiLA0KICAicmVzdWx0Ijogew0KICAgICJyZXR1cm5fY29kZSI6ICJPSyINCiAgfQ0KfQ==")
-    }
-
-    func testInitIpPort() throws {
-        let hoge = ZanoWallet.InitIpPort("", "", "", 0).data(using: .utf8)?.base64EncodedString()
-        XCTAssertEqual(hoge, "ew0KICAiZXJyb3IiOiB7DQogICAgImNvZGUiOiAiSU5URVJOQUxfRVJST1IiLA0KICAgICJtZXNzYWdlIjogIltpbml0XSBAIFwvVXNlcnNcL3Jva3lcL3Byb2plY3RzXC9aYW5vXC9tb2JpbGVfcmVwb1wvbW9iaWxlXC96YW5vX25hdGl2ZV9saWJcL3phbm9cL3NyY1wvd2FsbGV0XC9wbGFpbl93YWxsZXRfYXBpLmNwcDoyMzcgXG5tZXNzYWdlOlJlYWQtb25seSBmaWxlIHN5c3RlbSINCiAgfSwNCiAgImlkIjogMCwNCiAgImpzb25ycGMiOiAiIiwNCiAgIm1ldGhvZCI6ICIiDQp9")
-    }
-
 }
